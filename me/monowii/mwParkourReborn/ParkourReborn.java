976b6204261e81393e30eac2b743114812750c15
monowii <monowii@hotmail.fr>
2014-12-28 13:50:00 +0100

    Added info sign

--- me/monowii/mwParkourReborn/ParkourReborn.java
+++ me/monowii/mwParkourReborn/ParkourReborn.java
@@ -3,6 +3,7 @@ package me.monowii.mwParkourReborn;
 import me.monowii.mwParkourReborn.commands.ParkourCommand;
 import me.monowii.mwParkourReborn.listeners.PlayerMoveListener;
 import me.monowii.mwParkourReborn.listeners.PlayerQuitListener;
+import me.monowii.mwParkourReborn.listeners.SignChangeListener;
 import me.monowii.mwParkourReborn.managers.ParkoursManager;
 
 import org.bukkit.plugin.PluginManager;
@@ -20,6 +21,7 @@ public class ParkourReborn extends JavaPlugin
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvents(new PlayerMoveListener(), this);
 		pm.registerEvents(new PlayerQuitListener(), this);
+		pm.registerEvents(new SignChangeListener(), this);
 		
 		getCommand("parkour").setExecutor(new ParkourCommand());
 	}
---
+++ me/monowii/mwParkourReborn/listeners/SignChangeListener.java
@@ -0,0 +1,47 @@
+package me.monowii.mwParkourReborn.listeners;
+
+import me.monowii.mwParkourReborn.Parkour;
+import me.monowii.mwParkourReborn.managers.ParkoursManager;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.SignChangeEvent;
+
+public class SignChangeListener implements Listener
+{
+	@EventHandler
+	public void onSignChange(SignChangeEvent e)
+	{
+		Player p = e.getPlayer();
+		
+		if (e.getLine(0).equalsIgnoreCase("[pk]"))
+		{
+			if (p.hasPermission("mwParkourReborn.admin"))
+			{
+				if (e.getLine(1).equalsIgnoreCase("info"))
+				{
+					Parkour parkour = ParkoursManager.getParkour(e.getLine(2));
+					
+					if (parkour != null)
+					{
+						e.setLine(0, "§8[mwParkourRe]");
+						e.setLine(1, "§3Info");
+						e.setLine(2, parkour.getName() + " by");
+						e.setLine(3, parkour.getAuthors());
+					}
+					else
+					{
+						p.sendMessage("§cThis parkour don't exist !");
+						e.setCancelled(true);
+					}
+				}
+			}
+			else
+			{
+				p.sendMessage("§cYou don't have permission to place parkour sign !");
+				e.setCancelled(true);
+			}
+		}
+	}
+}
